1096bf380c0c6fe25b26761f30683665cb9be3a6
Gustavo Meza <gus.elim2@gmail.com>
2020-12-02 21:59:40 -0600

    Archivos: PasswordContext.js y useDatabase.js

---
+++ src/context/PasswordContext.js
@@ -0,0 +1,13 @@
+import React, {} from "react";
+import {database} from "../components/db";
+
+//Creacion del contexto
+export const NotesContext = CreateContext({});
+
+export const NotesContextProvider =(props)=>
+/*
+    Obetener
+*/
+{
+
+}
