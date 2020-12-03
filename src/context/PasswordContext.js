a5b7dcaa7a3aa88f5e1c4caee8c45e4a217acf52
Gustavo Meza <gus.elim2@gmail.com>
2020-12-02 22:18:32 -0600

    Creacion del archivo de contexto y para obtener algunos valores

--- src/context/PasswordContext.js
+++ src/context/PasswordContext.js
@@ -1,13 +1,45 @@
-import React, {} from "react";
+import React, { useEffect, createContext ,useState } from "react";
 import {database} from "../components/db";
 
 //Creacion del contexto
-export const NotesContext = CreateContext({});
+export const PasswordContext = createContext({});
 
-export const NotesContextProvider =(props)=>
-/*
-    Obetener
-*/
+export const PasswordContextProvider =(props)=>
 {
+/*
+    Obetener los valores iniciales del contexto
+    se obtienen desde los props
+*/
+    const {passwords: initialPassword, children} = props;
 
-}
+    //Almacenar los valores en el estado
+    const [passwords, setPasswords] = useState(initialPassword);
+
+    //Cargar las contraseñas
+    useEffect(() => {
+        refreshPasswords();
+    },[]);
+
+    const refreshPasswords =()=>
+    {
+        return database.getPassword(setPasswords);
+    };
+
+    const addNewPassword =(passwords)=>
+    {
+        return database.insertPassword(passwords,refreshPasswords);
+    };
+
+    //Objeto de contexto
+    const passwordContext ={
+        passwords,
+        addNewPassword,
+    };
+
+    //Pasar los valores
+    return(
+        <NotesContext.Provider value={PasswordContext}>
+            {children}
+        </NotesContext.Provider>
+    );
+};
