59e9b46510d104ef64a3f6d92798faecf0ada2a7
Gustavo Meza <gus.elim2@gmail.com>
2020-12-03 20:02:27 -0600

    Implementando diseño en la primera pantalla, usando z index y dandole forma circular a una punta

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,49 @@
-import {Content,H1} from "native-base";
+import {Container,  Header, Content, Card, CardItem, Body,H1,Text,View} from "native-base";
 import React , {useEffect, useState} from "react";
-import {Button } from "react-native";
+import {Dimensions,Button } from "react-native";
+
+const {width, height} = Dimensions.get("window");
+
 const Home = ({navigation})=>
 {
     return(
-        <Content>
-            <H1>HOME</H1>
-            <Button title="Form" onPress={ () => navigation.navigate('DoberkeyFormScreen',{})} />
-            <Button title="View" onPress={ () => navigation.navigate('DoberkeyViewScreen',{})} />
-        </Content>
+        <View style={{flex:1, backgroundColor:'#fff'}} >
+            
+            <View style={{flex:0.3,alignItems:'center',padding:'5%',borderBottomLeftRadius:40 ,backgroundColor:'#5E5C00'}}>
+                <Text style={{color:'#fff',fontSize:20,margin:'3%' ,fontWeight:'bold' }} >Hola, Lorem Impun</Text>
+                <Text style={{color:'#B6B27F', margin:'4%'}} >3 Claves Guardadas</Text>
+                <Button color="#CAA648" title="Añadir Clave"></Button>
+            </View>
+
+            <View style={{flex:1,position:"relative" ,flexDirection:'row-reverse' ,backgroundColor:'#fff'}}>
+                
+                <View style={{right:0,zIndex:-2,position:'relative' ,width:50,height:50, backgroundColor:'#5E5C00'}} >
+                    <View style={{right:0,zIndex:1,position:'relative',borderTopRightRadius:90 ,width:50,height:50, backgroundColor:'#fff'}} >    
+                    </View>
+                </View>
+            
+            
+            <Content style={{flex:1, zIndex:3,width:width,position:'relative',backgroundColor:'transparent',flexDirection:'column' }} >
+                <Content style={{margin:'10%'}} >
+                <Card>
+                    <CardItem header bordered>
+                        <Text>NativeBase</Text>
+                    </CardItem>
+                        <CardItem>
+                        <Body>
+                            <Text>
+                            //Your text here
+                            </Text>
+                        </Body>
+                        </CardItem>
+                </Card>
+                </Content>
+            </Content>
+
+            </View>
+
+           
+        </View>
     )
 }
 
