8d1553fa66567a86a2125d5a5a731ee7299e0904
Gustavo Meza <gus.elim2@gmail.com>
2020-12-06 19:36:06 -0600

    Algunos estilos agragados a la variable de estilo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -24,48 +24,40 @@ const Home = ({navigation})=>
       }
 
         return(
-            <View style={{flex:1, backgroundColor:'#fff'}} >
+            <View style={styles.contenedorPrincipal} >
                 
-                <View style={{flex:0.3,alignItems:'center',padding:'5%',borderBottomLeftRadius:40 ,backgroundColor:'#5E5C00'}}>
-                    <Text style={{color:'#fff',fontSize:20,margin:'3%' ,fontWeight:'bold' }} >Hola, ¿cuantas guardaremos?</Text>
+                <View style={styles.topGreen}>
+                    <Text style={styles.saludo} >Hola, ¿cuantas guardaremos?</Text>
                     <Text style={{color:'#B6B27F', margin:'4%'}} > {passwords.length} Claves Guardadas</Text>
                     <Button color="#CAA648" title="Añadir Clave"></Button>
                 </View>
     
-                <View style={{flex:1,position:"relative" ,flexDirection:'row-reverse' ,backgroundColor:'#fff'}}>
+                <View style={styles.contenedorMiddle}>
                     
-                    <View style={{right:0,zIndex:-2,position:'relative' ,width:50,height:50, backgroundColor:'#5E5C00'}} >
-                        <View style={{right:0,zIndex:1,position:'relative',borderTopRightRadius:40 ,width:50,height:50, backgroundColor:'#fff'}} >    
+                    <View style={styles.verdeIndex} >
+                        <View style={styles.blancoIndex} >    
                         </View>
                     </View>
                 
                 
-                <Content  style={{flex:1,zIndex:3,width:width,position:'relative',backgroundColor:'transparent',flexDirection:'column' }} >
-                    <List style={{margin:'10%'}} >
+                <Content  style={styles.contenedorTarjetas} >
+                    <List style={styles.listaMargen} >
                     {passwords ? passwords.map((password)=>(
-                        <Card key={password.id.toString()} style={{backgroundColor:'#ffffff', borderRadius:20,shadowColor: "#000",
-                        shadowOffset: {
-                            width: 0,
-                            height: 12,
-                        },
-                        shadowOpacity: 0.58,
-                        shadowRadius: 16.00,
-                        
-                        elevation: 24,marginBottom:'10%'}} >
-                            <CardItem  bordered style={{top:10 ,borderColor:'#CE7B1C', borderRadius:20}} >
-                                        <Text  style={{fontSize:20,height:30 ,fontWeight:'bold'}}>
+                        <Card key={password.id.toString()} style={styles.elementoCard} >
+                            <CardItem  bordered style={styles.itemSitio} >
+                                        <Text  style={styles.sitio}>
                                             {password.nombreDelSitio} {"\n"}
                                         </Text>
                             </CardItem>
                             
-                            <CardItem style={{ borderRadius:20, marginTop:10}} >
+                            <CardItem style={styles.detalles} >
                                 <Body >
                                 <Text  >
-                                    <Text style={{fontWeight:'bold'}} >
+                                    <Text style={styles.detalle} >
                                         Usuario: 
                                     </Text>
                                     {password.usuario} {"\n"}
-                                    <Text style={{fontWeight:'bold'}} >
+                                    <Text style={styles.detalle} >
                                         Observacion: 
                                     </Text>
                                     {password.observaciones}
@@ -90,6 +82,98 @@ const styles = StyleSheet.create({
     {
         flex: 1, 
         justifyContent: "center" 
+    },
+    contenedorPrincipal:
+    {
+        flex:1, 
+        backgroundColor:'#fff'
+    },
+    topGreen:
+    {   flex:0.3, 
+        alignItems:'center',
+        padding:'5%',
+        borderBottomLeftRadius:40,
+        backgroundColor:'#5E5C00'
+    },
+    saludo:
+    {
+        color:'#fff',
+        fontSize:20,
+        margin:'3%' ,
+        fontWeight:'bold' 
+    },
+    contenedorMiddle:
+    {
+        flex:1,position:"relative" ,
+        flexDirection:'row-reverse' ,
+        backgroundColor:'#fff'
+    },
+    verdeIndex:
+    {
+        right:0,
+        zIndex:-2,
+        position:'relative' ,
+        width:50,
+        height:50, 
+        backgroundColor:'#5E5C00'
+    },
+    blancoIndex:
+    {
+        right:0,
+        zIndex:1,
+        position:'relative',
+        borderTopRightRadius:40 ,
+        width:50,
+        height:50, 
+        backgroundColor:'#fff'
+    },
+    contenedorTarjetas:
+    {
+        flex:1,
+        zIndex:3,
+        width:width,
+        position:'relative',
+        backgroundColor:'transparent',
+        flexDirection:'column'
+    },
+    listaMargen:
+    {
+        margin:'10%'
+    },
+    elementoCard:
+    {
+        backgroundColor:'#ffffff', 
+        borderRadius:20,
+        shadowColor: "#000",
+        shadowOffset: 
+            {
+                width: 0,
+                height: 12,
+            },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,            
+        elevation: 24,marginBottom:'10%'
+    },
+    itemSitio:
+    {
+        top:10 ,
+        borderColor:'#CE7B1C', 
+        borderRadius:20
+    },
+    sitio:
+    {
+        fontSize:20,
+        height:30 ,
+        fontWeight:'bold'
+    },
+    detalles:
+    {
+        borderRadius:20, 
+        marginTop:10
+    },
+    detalle:
+    {
+        fontWeight:'bold'
     },
 });
 
